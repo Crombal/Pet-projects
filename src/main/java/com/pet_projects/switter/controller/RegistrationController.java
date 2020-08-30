30d1ee731b4d2ad2a570eae264a11f3b0b7c8595
Anton Treushchenko <anton.treushchenko@gmail.com>
2020-08-31 00:35:06 +0300

    Переход с шаблонизатора Mustache на Freemarker. Упрощение кода.

--- src/main/java/com/pet_projects/switter/controller/RegistrationController.java
+++ src/main/java/com/pet_projects/switter/controller/RegistrationController.java
@@ -5,11 +5,11 @@ import com.pet_projects.switter.domain.User;
 import com.pet_projects.switter.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
-import java.util.Map;
 
 @Controller
 public class RegistrationController {
@@ -22,11 +22,11 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
+    public String addUser(User user, Model model) {
         User userFromDB = userRepository.findByUsername(user.getUsername());
 
         if (userFromDB != null) {
-            model.put("message", "User exists!");
+            model.addAttribute("message", "User exists!");
             return "registration";
         }
 
