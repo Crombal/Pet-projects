c0a13947f9ce973beddbd8ef87653cf0e5b181de
Anton Treushchenko <anton.treushchenko@gmail.com>
2020-08-31 01:37:53 +0300

    Добавлена панель администратора.

--- src/main/java/com/pet_projects/switter/domain/Role.java
+++ src/main/java/com/pet_projects/switter/domain/Role.java
@@ -3,7 +3,7 @@ package com.pet_projects.switter.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
