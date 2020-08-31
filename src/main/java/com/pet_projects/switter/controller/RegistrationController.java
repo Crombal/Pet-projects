5b33d44e74c56a057468ef8a878daf65935aa5fb
Anton Treushchenko <anton.treushchenko@gmail.com>
2020-08-31 13:40:10 +0300

    Добавлена рассылка почты пользователям, активация аккаунта.

--- src/main/java/com/pet_projects/switter/controller/RegistrationController.java
+++ src/main/java/com/pet_projects/switter/controller/RegistrationController.java
@@ -1,20 +1,18 @@
 package com.pet_projects.switter.controller;
 
-import com.pet_projects.switter.domain.Role;
 import com.pet_projects.switter.domain.User;
-import com.pet_projects.switter.repository.UserRepository;
+import com.pet_projects.switter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -23,17 +21,25 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Model model) {
-        User userFromDB = userRepository.findByUsername(user.getUsername());
 
-        if (userFromDB != null) {
+        if (!userService.addUser(user)) {
             model.addAttribute("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
-
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable String code, Model model) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated!");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
     }
 }
